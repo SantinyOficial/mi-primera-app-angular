5ed45a245717d26862d15c77a6f07df78b607a76
SantinyOficial <santiunido@gmail.com>
2024-08-17 07:10:21 -0500

    directiva ngIf

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 })
 export class PersonasComponent {
   deshabilitar = false;
-  mensaje: string = 'No se ha agregado ninguna persona';
+  mensaje: string = '';
   titulo = '';
+  mostrar: boolean = false;
 
   agregarPersona() {
+    this.mostrar = true;
     this.mensaje = 'Persona agregada';
   }
 
